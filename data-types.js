b66e0352b789da8b8db4179f6ff365dd7fd3ec5e
23EIR022 <dhanusripgm@gmail.com>
2025-06-20 12:15:59 +0530

    complete strings

--- data-types.js
+++ data-types.js
@@ -3,3 +3,8 @@ let age = 19;
 let price = 150.23;
 let isStudent = false;
 console.log(name);
+
+// Template strings
+let newString = `My name is ${name}`;
+console.log(newString);
+console.log(`$ ${price}`);
